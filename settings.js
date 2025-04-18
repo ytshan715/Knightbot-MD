ceed7a00844a4ea4e80b2a3c31b0293f189df1a1
ytshan715 <bandarashanuka933@gmail.com>
2025-04-18 13:44:46 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,10 +2,10 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
-  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  botOwner: 'Professor', //Shanuka
+  ownerNumber: '917023951514', //0740712026
+  giphyApiKey: '',
+  commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "1.0.0",
 };
